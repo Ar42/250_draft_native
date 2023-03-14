298ff1005b542f768d976765579e8ea692b06e25
Ar42 <arnobhasan364@gmail.com>
2023-03-15 00:13:44 +0600

    react native front end

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { QueryClientProvider, QueryClient } from "react-query";
+
+import PostCreate from "./src/screen/postCreate";
+import BlogDetails from "./src/screen/blogDetails";
+import Blogs from "./src/screen/Blogs";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const queryClient = new QueryClient();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <View style={styles.container}>
+          {/* <Stack.Navigator initialRouteName="Blog Details"> */}
+          <Stack.Navigator initialRouteName="Post Create">
+            <Stack.Screen name="Post Create" component={PostCreate} />
+            <Stack.Screen name="Blogs" component={Blogs} />
+            <Stack.Screen name="Blog Details" component={BlogDetails} />
+          </Stack.Navigator>
+        </View>
+      </QueryClientProvider>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: "#000",
   },
 });
